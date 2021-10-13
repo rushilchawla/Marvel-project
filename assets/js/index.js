3d3fa27a5d5d51f51a2eb6578dad37c91f32e51c
Aaron Sando <Aaron@Charactercabinets.com.au>
2021-10-13 15:09:57 +0800

    changes to all files AS

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,30 +1,34 @@
 var contentDropdownEl = document.getElementById("content-dropdown");
 var characterNameEl = document.getElementById("character-name");
 var characterTitleEl = document.getElementById("character-title");
-var characterDescEl = document.getElementById("character-desc");
 
 //API key
 const apiKey = "879eacf85a9c70745a7fb038ce0cca14";
 
 // required details as per https://developer.marvel.com/documentation/authorization
 const ts = +new Date();
-console.log("TIME", ts);
+//console.log("TIME", ts);
 const privateKey = "48df6dafa89bd8c2666981b77990fe8216f6f14c";
 
 const hash = CryptoJS.MD5(ts + privateKey + apiKey).toString();
-console.log("HASH", hash);
+//console.log("HASH", hash);
 
 var characterID = localStorage.getItem("characterID");
-console.log(characterID);
+//console.log(characterID);
 
 //set varabiles for use with JavaScript
 var character;
 var characterDesc;
+var characterDescEl = document.getElementById("character-desc");
 var characterImage;
-var comics;
-var comicsImages;
-var comicName
 var characterImageEl = document.getElementById("character-image");
+var characterDescWebsite;
+var characterDescWebsiteEl = document.getElementById("character-desc-website");
+var characterEvents;
+var comics;
+var comicImage;
+var comicImageEl = document.getElementById("comic-image");
+var comicName;
 var comicNameEl = document.getElementById("comic-name");
 
 // add fetch to get character image and discription
@@ -35,61 +39,87 @@ async function getContent(characterName) {
   if (response.ok) {
 
     let json = await response.json();
+    //set vaules for variables as per the json response
     character = json.data.results[0];
     characterDesc = json.data.results[0].description;
     characterImage = json.data.results[0].thumbnail.path;
+    characterDescWebsite = "To access "+json.data.results[0].name+"'s Marvel Profile Page click here";
 
-    $(characterDescEl).empty()
-    characterDescEl.append(characterDesc)
+    //add link to Marvels profile page for character selected
+    $(characterDescWebsiteEl).empty()
+    characterDescWebsiteEl.append(characterDescWebsite)
+    characterDescWebsiteEl.onclick = function() {
+      window.open(`${json.data.results[0].urls[1].url}.wiki`,'_blank');
+      window.open(`${json.data.results[0].urls[1].url}.wiki`);
+    };
 
+    // make element for appending
     const liCharacterEl = document.createElement("li");
     const imgEl = document.createElement("img");
-
+    
+    //add image of character
     $(characterImageEl).empty()
     imgEl.src = `${characterImage}.jpg`;
+    imgEl.height = 450;
+    imgEl.width = 450;
     liCharacterEl.appendChild(imgEl);
     characterImageEl.appendChild(liCharacterEl);
 
+    // add discription of character
+    $(characterDescEl).empty()
+    characterDescEl.append(characterDesc)
+
     // 2nd fetch to get comic API
     let response2 = await fetch(
-      `https://gateway.marvel.com/v1/public/characters/${character.id}/comics?apikey=${apiKey}&hash=${hash}&ts=${ts}&limit=5`
+      `https://gateway.marvel.com/v1/public/characters/${character.id}/comics?apikey=${apiKey}&hash=${hash}&ts=${ts}&limit=10`
     );
     if (response2.ok) {
 
       let json2 = await response2.json();
+      //set vaules for variable as per the json response
       comics = json2.data.results;
-      // add comic names
+      // add comic names, images and hyperlink
       $(comicNameEl).empty()
+      $(comicImageEl).empty()
 
-      for (let i = 0; i < 4; i++) {
+      for (let i = 0; i < 10; i++) {
 
         comicName = json2.data.results[i].series.name;
-        
-        const liComicNameEl = document.createElement("li");
-   
-        comicNameEl.append(liComicNameEl);
-        liComicNameEl.append(comicName);
+        comicImage = json2.data.results[i].thumbnail.path;
+
+        //const liComicNameEl = document.createElement("li");
+        const imgComicEl = document.createElement("img");
+        // add comimc image and hyperlink them
+        imgComicEl.src = `${comicImage}.jpg`;
+        imgComicEl.height = 350;
+        imgComicEl.width = 185;
+        imgComicEl.onerror="this.src='https://www.unesale.com/ProductImages/Large/notfound.png'"
+        imgComicEl.onclick = function() {
+          window.open(`${json2.data.results[i].urls[0].url}`,'_blank');
+          window.open(`${json2.data.results[i].urls[0].url}`);
+        };
+
+        comicImageEl.append(imgComicEl);
+        //comicNameEl.append(liComicNameEl);
+        //liComicNameEl.append(comicName);             
 
       }
-     
-      // add comimc image and hyperlink them
-      //const liComicNameEl = document.createElement("li");
+      // // 3rd fetch to get event API
+      // let response3 = await fetch(
+      //   `https://gateway.marvel.com/v1/public/characters/${character.id}/events?apikey=${apiKey}&hash=${hash}&ts=${ts}&limit=50`
+      // );
+      // if (response3.ok) {
 
-      // add comic image
-      //comicImage = json2.data.results[0].images[0].path;
-      //comicImage = json2.data.results[1].images[0].path;
+      //   let json3 = await response3.json();
+      //   //set vaules for variables as per the json response
+      //   characterEvents = json3.data.results;
 
-      //const liComicEl = document.createElement("li");
-      //const imgComicEl = document.createElement("img");
-
-      //$(comicImageEl).empty()
-      //imgComicEl.src = `${comicImage}.jpg`;
-      //liComicEl.appendChild(imgComicEl);
-      //comicImageEl.appendChild(liComicEl);
-
-    } else {
-      alert("HTTP-Error: " + response2.status);
-    }
+      // } else {
+      //   alert("HTTP-Error: " + response3.status);
+      // }
+      } else {
+        alert("HTTP-Error: " + response2.status);
+      }
   } else {
     alert("HTTP-Error: " + response.status);
   }
@@ -99,13 +129,13 @@ async function getContent(characterName) {
 // save selected character to local storage
 function init() {
   var savedCharacter = localStorage.getItem("characterName");
-  console.log(savedCharacter);
+//  console.log(savedCharacter);
   if (savedCharacter !== []) {
     localStorage.setItem("characterName", savedCharacter);
     characterNameEl.value = localStorage.getItem("characterName");
 
     getContent(savedCharacter);
-    console.log(savedCharacter);
+//    console.log(savedCharacter);
   } else {
     characterTitleEl.innerHTML = "";
   }
