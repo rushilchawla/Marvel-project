c0a41f3285abccb7b0eeb66c5e1524423731d705
Aaron Sando <Aaron@Charactercabinets.com.au>
2021-10-13 09:35:05 +0800

    updated and cleaned code

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,12 @@
-//var searchBtn = document.getElementById("search-button")
 var contentDropdownEl = document.getElementById("content-dropdown");
 var characterNameEl = document.getElementById("character-name");
 var characterTitleEl = document.getElementById("character-title");
 var characterDescEl = document.getElementById("character-desc");
 
-console.log(characterNameEl.value);
-
+//API key
 const apiKey = "879eacf85a9c70745a7fb038ce0cca14";
 
+// required details as per https://developer.marvel.com/documentation/authorization
 const ts = +new Date();
 console.log("TIME", ts);
 const privateKey = "48df6dafa89bd8c2666981b77990fe8216f6f14c";
@@ -18,6 +17,7 @@ console.log("HASH", hash);
 var characterID = localStorage.getItem("characterID");
 console.log(characterID);
 
+//set varabiles for use with JavaScript
 var character;
 var characterDesc;
 var characterImage;
@@ -25,16 +25,15 @@ var comics;
 var comicsImages;
 var comicName
 var characterImageEl = document.getElementById("character-image");
-//var comicImageEl = document.getElementById("comic-image");
 var comicNameEl = document.getElementById("comic-name");
 
+// add fetch to get character image and discription
 async function getContent(characterName) {
   let response = await fetch(
     `https://gateway.marvel.com/v1/public/characters?apikey=${apiKey}&hash=${hash}&ts=${ts}&limit=100&name=${characterName}`
   );
   if (response.ok) {
-    // if HTTP-status is 200-299
-    // get the response body (the method explained below)
+
     let json = await response.json();
     character = json.data.results[0];
     characterDesc = json.data.results[0].description;
@@ -51,27 +50,30 @@ async function getContent(characterName) {
     liCharacterEl.appendChild(imgEl);
     characterImageEl.appendChild(liCharacterEl);
 
+    // 2nd fetch to get comic API
     let response2 = await fetch(
       `https://gateway.marvel.com/v1/public/characters/${character.id}/comics?apikey=${apiKey}&hash=${hash}&ts=${ts}&limit=5`
     );
     if (response2.ok) {
-      // if HTTP-status is 200-299
-      // get the response body (the method explained below)
+
       let json2 = await response2.json();
       comics = json2.data.results;
       // add comic names
       $(comicNameEl).empty()
 
       for (let i = 0; i < 4; i++) {
-         comicName = json2.data.results[i].series.name;
-      comicNameEl.append(comicName);
-        }
-     
 
+        comicName = json2.data.results[i].series.name;
+        
+        const liComicNameEl = document.createElement("li");
+   
+        comicNameEl.append(liComicNameEl);
+        liComicNameEl.append(comicName);
+
+      }
+     
+      // add comimc image and hyperlink them
       //const liComicNameEl = document.createElement("li");
-
-     
-      
 
       // add comic image
       //comicImage = json2.data.results[0].images[0].path;
@@ -94,14 +96,13 @@ async function getContent(characterName) {
 
 }
 
+// save selected character to local storage
 function init() {
   var savedCharacter = localStorage.getItem("characterName");
   console.log(savedCharacter);
   if (savedCharacter !== []) {
     localStorage.setItem("characterName", savedCharacter);
     characterNameEl.value = localStorage.getItem("characterName");
-    characterTitleEl.innerHTML =
-      characterNameEl.options[characterNameEl.selectedIndex].text;
 
     getContent(savedCharacter);
     console.log(savedCharacter);
